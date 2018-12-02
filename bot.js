cc2ad66a26a31bd601e8ccdf19d9c7df4cea74bd
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-02 11:18:45 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,222 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+client.login('NTE4MTQ5NDc0ODgzOTkzNjAy.DuW9hQ.T0r8v-RaSgVIuQJAi_uxRm5bzXc'); 
+
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by mohamed');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by mohamed');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
+
+client.on('message', message => {
+     if (message.content === "ms") {
+message.author.send("**لـ ارسال رسالة لجميع الاعضاء : !bc**" + `  **
+ل عمل تيكيت :!new 
+لمسح الشات !مسح وعدد الرسائل المراد مسحها 
+لـ قفل التيكيت !close  
+حصول علي رتبه ريمبو  يجب اولا تواصل مع اونر السيرفر للحصول عليها 
+لـ رابط دعوه البوت : !invite
+لـ عمل كيك لاي عضو : !kick
+لـ عمل باند لاي عضو : !ban 
+لـ عمل قرعة : -roll
+لـ عرض معلومات الروم  : -ch
+لـ قفل الشات : -قفل
+ل فتح الشات : -فتح
+
+**`);
+    }
+});    
+client.on('message', message => {
+    if(command === "مسح") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الكلمات التي تريد مسحها```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+var prefix = "-";
+if(message.content.startsWith(prefix + 'ch')) {
+    let channel = message.channel
+    var embed = new Discord.RichEmbed()
+      .setTitle("معلومات الروم")
+      .setColor("RANDOM")
+      .setDescription(`ايدي الروم <#${channel.id}>\nChannel ID: ${channel.id}`)
+      .addField("صنع في", `${channel.createdAt}`)
+      .addField("نوع الروم", `${channel.type}`)
+      .addField("Extra Information", `Channel is NSFW => ${channel.nsfw}\nChannel Topic=> ${channel.topic}\nChannel Parent => ${channel.parent}\nChannel Position => ${channel.position}`)
+ 
+     message.channel.send({ embed: embed });
+  }
+ 
+    });
+    
+	client.on("ready", () => {
+  function lol() {
+    client.guilds.get('441615240006729728').roles.find("name", "Rainbow").setColor("RANDOM");
+  };
+  setInterval(lol, 1600);
+});
+
+client.on('message', message => {
+var prefix = "-";
+      if(message.content === prefix + "قفل") {
+      if(!message.channel.guild) return;
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You Dont Have Perms ❌');
+             message.channel.overwritePermissions(message.guild.id, {
+             READ_MESSAGES: false
+ })
+              message.channel.send('تم اخفاء القناه , لا يمكن لاي عضو كتابه اي شئ ! ✅  ')
+ }
+});
+
+
+client.on('message', message => {
+var prefix = "-";
+      if(message.content === prefix + "فتح") {
+      if(!message.channel.guild) return;
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('❌');
+             message.channel.overwritePermissions(message.guild.id, {
+             READ_MESSAGES: true
+ })
+              message.channel.send('تم فتح القناه , يمكن لاي عضو ارسال رسالة والكتابه ✅ ')
+ }
+});
+
+client.on('message', message => {
+	var prefix = "-"
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .kickable) return message.reply("**لايمكنني طرد هذا العضو لانه يملك رتبه اعلي من رتبه البوت**");
+
+  message.guild.member(user).kick();
+
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+  })
+}
+});
+
+client.on('message', message => {
+	var prefix = "-"
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .kickable) return message.reply("**لايمكنني طرد هذا العضو لانه يملك رتبه اعلي من رتبه البوت**");
+
+  message.guild.member(user).kick();
+
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+  })
+}
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:pray: نورت السيرفر ياحب , انشاء الله تستمتع معانا وتكون ف مقدمة السيرفر :pray:  
+:radioactive: اسمك :  ${member}:radioactive:   
+انت العضو رقم  ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+client.on('message', message => {
+  if (true) {
+if (message.content === '-invite') {
+      message.author.send('  https://discordapp.com/oauth2/authorize?client_id=515951600154705960&scope=bot&permissions=36760572|  رابط دعوه مالتي بوت     ').catch(e => console.log(e.stack));
+
+    }
+   } 
+  });
+  var prefix ='-'
+ client.on('message', function(message) {
+    if(message.content.startsWith(prefix + 'roll')) {
+        let args = message.content.split(" ").slice(1);
+        if (!args[0]) {
+            message.channel.send('**Put a number**');
+            return;
+            }
+    message.channel.send(Math.floor(Math.random() * args.join(' ')));
+            if (!args[0]) {
+          message.edit('1')
+          return;
+        }
+    }
+});
