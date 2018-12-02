eb25c4c5e19b68bbc4d4533e217f9e6cdf933a83
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-02 11:57:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
   console.log('by mohamed');
