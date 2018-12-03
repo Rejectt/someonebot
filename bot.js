a0a9860bad39aa3e6e7d753724e5d5251ce15a00
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-03 09:55:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -363,4 +363,5 @@ const devs = ['441615240006729728'];
     }
 });
 
+client.on('message', message => { if (message.content === "-id") { let embed = new Discord.RichEmbed() .setColor("RANDOM") .setThumbnail(message.author.avatarURL) .addField("Name:",`${message.author.username}`, true) .addField('Discrim:',"#" + message.author.discriminator, true) .addField("ID:", message.author.id, true) .addField("Create At:", message.author.createdAt, true) message.channel.sendEmbed(embed); } });
 client.login(process.env.BOT_TOKEN);
